3528fd5efaf67feb332bce507b6da7c403cd6fe7
midatindex0 <midatindex0@users.noreply.github.com>
2023-12-18 08:16:13 +0530

    new access control

--- consts.py
+++ consts.py
@@ -1,7 +1,7 @@
 MAX_FILE_SIZE = 32 * 1024 * 1024  # 32mb
 CDN_ROUTE = "/cdn"
 ORIGINS = [
-    "http://localhost:8000",  # This server
-    "http://localhost:5173",  # Relay server
+    "http://localhost:5173",  # Local frontend
+    "https://dreamh.vercel.app",
 ]
 MAX_SESSIONS = 5
